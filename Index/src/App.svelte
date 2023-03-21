42820af1e6a65064dddfe871f3df0fa889ce7993
Saksham <sakshamdev3@gmail.com>
2023-03-21 12:21:09 +0530

    ROOM BOX COMPONENT MADE and Added to Index

--- Index/src/App.svelte
+++ Index/src/App.svelte
@@ -1,29 +1,49 @@
+<script>
+  import Box from "./Box.svelte";
+  var toggle = document.getElementById("toggle");
+  var overlay = document.getElementById("overlay");
+
+  // toggle.addEventListener("click", function () {
+  //   toggle.classList.toggle("active");
+  //   overlay.classList.toggle("open");
+  // });
+
+  function onClick() {
+    console.log("HELLO");
+    toggle.classList.toggle("active");
+    overlay.classList.toggle("open");
+  }
+</script>
+
 <body>
   <header>
     <div class="header">
-    
-      <img src="CompanyLogo.png" alt='Comapany Logo' class="logo"/>
-        <div class="header-right">
+      <img src="CompanyLogo.png" alt="Comapany Logo" class="logo" />
+      <div class="header-right">
         <a class="active" href="#home">Home</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
       </div>
     </div>
-    
+
     <!-- Hamburger -->
-    <div class="button_container" id="toggle"><span class="top"></span><span class="middle"></span><span class="bottom"></span></div>
+    <div class="button_container" id="toggle" on:click={onClick}>
+      <span class="top" /><span class="middle" /><span class="bottom" />
+    </div>
     <div class="overlay" id="overlay">
       <nav class="overlay-menu">
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
+          <li><a href="#">Rooms</a></li>
+          <li><a href="#">All Assignment</a></li>
           <li><a href="#">Work</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
       </nav>
     </div>
-
   </header>
-
-
+  
+  <BOX></BOX>
 </body>
+
+<style>
+</style>
---
+++ Room Box/room box/src/Box.svelte
@@ -0,0 +1,78 @@
+<script>
+    export let room_box_name='Room Box';
+	export let room_box_image='./room_box_image.png'
+</script>
+
+<div class="RoomBox" id="RoomBox">
+	<a href="#RemoveRoomFunc" class="close"> </a>
+	<div class="room_box_name" >
+		{room_box_name}
+	</div>
+	<div class="room_box_image">
+		<img src={room_box_image} alt="ICON" />
+	</div>
+</div>
+
+<style>
+	.RoomBox {
+		position: relative;
+	width: 300px;
+	height: 300px;
+	background: rgba(255, 87, 87, 0.76);
+	border-radius: 25px;
+	margin: 20px;
+	display: inline-block;
+	
+	}
+	.room_box_name
+	{
+		position: relative;
+		
+		font-family: 'Open Sans';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 30px;
+		text-align: center;
+		top: 75%;
+		color: #FFFFFF;
+		
+	}
+
+	.room_box_image
+	{
+		text-align: center;
+	
+	}
+	.room_box_image img
+	{	
+		height: 150px;
+		
+	}
+	
+	.close {
+
+	width: 32px;
+	height: 32px;
+	opacity: 0.3;
+	
+	}
+	.close:hover {
+	opacity: 1;
+	}
+	.close:before, .close:after {
+	position: absolute;
+	right: 15px;
+	content: ' ';
+	height: 33px;
+	width: 2px;
+	background-color: #333;
+	margin: 10%;
+	}
+	.close:before {
+	transform: rotate(45deg);
+	}
+	.close:after {
+	transform: rotate(-45deg);
+	}
+
+</style>
